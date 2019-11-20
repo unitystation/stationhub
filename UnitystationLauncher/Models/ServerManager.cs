4e1524f847d942ee63afdc0af4c28724e16b15b5
Doobly <rob@smashingpixels.com.au>
2019-11-20 16:30:21 +1000

    Little clean up

--- UnitystationLauncher/Models/ServerManager.cs
+++ UnitystationLauncher/Models/ServerManager.cs
@@ -1,72 +1,74 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using ReactiveUI;
 using Serilog;
-using System.Text.Json;
-using System.Reactive.Subjects;
 using System.Reactive.Linq;
-using System.Reactive.Threading.Tasks;
+using Reactive.Bindings;
 
-namespace UnitystationLauncher.Models{
-
+namespace UnitystationLauncher.Models
+{
     public class ServerManager : ReactiveObject, IDisposable
     {
         private readonly HttpClient http;
-        private readonly BehaviorSubject<IReadOnlyList<ServerWrapper>> serversSubject;
         private AuthManager authManager;
-        TimeSpan refreshTimeout = TimeSpan.FromMinutes(1);
         bool refreshing;
 
         public ServerManager(HttpClient http, AuthManager authManager)
         {
-            serversSubject = new BehaviorSubject<IReadOnlyList<ServerWrapper>>(new ServerWrapper[0]);
-            Observable.Timer(TimeSpan.Zero, refreshTimeout)
-                .Do(x => Refreshing = true)
-                .Do(x => Log.Verbose("Refreshing server list..."))
-                .SelectMany(u => http.GetStringAsync(Config.apiUrl).ToObservable())
-                .Do(x => Refreshing = false)
-                .DistinctUntilChanged()
-                .Select(JsonConvert.DeserializeObject<ServerList>)
-                .Select(servers => AddTest(servers))
-                .Select(servers => servers.Servers.Select(s => new ServerWrapper(s, authManager)).ToList())
-                .Subscribe(serversSubject);
-
-
-               
             this.http = http;
             this.authManager = authManager;
+            RefreshServerList();
+        }
+
+        public async void RefreshServerList()
+        {
+            NoServersFound.Value = false;
+            if (Refreshing) return;
+
+            Servers.Value = new List<ServerWrapper>();
+            Refreshing = true;
+            Log.Verbose("Refreshing server list...");
+            var data = await http.GetStringAsync(Config.apiUrl);
+            var serverList = JsonConvert.DeserializeObject<ServerList>(data);
+            if (serverList.Servers.Count == 0)
+            {
+                NoServersFound.Value = true;
+            }
+            else
+            {
+               // AddTest(serverList);
+                List<ServerWrapper> updatedList = new List<ServerWrapper>();
+                foreach (Server s in serverList.Servers)
+                {
+                    updatedList.Add(new ServerWrapper(s, authManager));
+                }
+
+                Servers.Value = updatedList;
+            }
+            Refreshing = false;
         }
 
         private ServerList AddTest(ServerList list)
         {
-            list.Servers.Add(new Server { ServerName = "TEST", BuildVersion = 12125, ForkName="fawf", ServerIP="10.1.1.12", ServerPort=1234});
+            list.Servers.Add(new Server { ServerName = "TEST", BuildVersion = 12125, ForkName = "fawf", ServerIP = "10.1.1.12", ServerPort = 1234 });
             list.Servers.Add(new Server { ServerName = "TEST2", BuildVersion = 121425, ForkName = "f3123f", ServerIP = "10.1.1.13", ServerPort = 1214 });
             list.Servers.Add(new Server { ServerName = "TEST44", BuildVersion = 25, ForkName = "dawda2", ServerIP = "10.1.1.15", ServerPort = 1324 });
             return list;
         }
 
-        public IObservable<IReadOnlyList<ServerWrapper>> Servers => serversSubject;
-
+        public ReactiveProperty<List<ServerWrapper>> Servers { get; private set; } = new ReactiveProperty<List<ServerWrapper>>();
+        public ReactiveProperty<bool> NoServersFound { get; private set; } = new ReactiveProperty<bool>();
         public bool Refreshing
         {
             get => refreshing;
             set => this.RaiseAndSetIfChanged(ref refreshing, value);
         }
 
-        public TimeSpan RefreshTimeout
-        {
-            get => refreshTimeout;
-            set => this.RaiseAndSetIfChanged(ref refreshTimeout, value);
-        }
-
         public void Dispose()
         {
-            serversSubject.Dispose();
+            Servers.Dispose();
         }
     }
 }
