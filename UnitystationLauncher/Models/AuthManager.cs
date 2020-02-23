ab0f95514a7e232134d0ccc40f7c1828a8732f1f
Doobly <rob@smashingpixels.com.au>
2020-02-23 12:53:26 +1000

    Start of custom token validation

--- UnitystationLauncher/Models/AuthManager.cs
+++ UnitystationLauncher/Models/AuthManager.cs
@@ -3,18 +3,22 @@ using System.IO;
 using System.Threading.Tasks;
 using Firebase.Auth;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading;
 
 namespace UnitystationLauncher.Models
 {
     public class AuthManager
     {
         readonly FirebaseAuthProvider authProvider;
+        private readonly HttpClient http;
         public LoginMsg LoginMsg { get; set; }
         public bool AttemptingAutoLogin { get; set; }
-        
-        public AuthManager(FirebaseAuthProvider authProvider)
+
+        public AuthManager(HttpClient http, FirebaseAuthProvider authProvider)
         {
             this.authProvider = authProvider;
+            this.http = http;
             if (File.Exists("settings.json"))
             {
                 var json = File.ReadAllText("settings.json");
@@ -22,9 +26,8 @@ namespace UnitystationLauncher.Models
                 AuthLink = authLink;
             }
         }
-        
-        public FirebaseAuthLink? AuthLink { get; set; }
 
+        public FirebaseAuthLink? AuthLink { get; set; }
         public string CurrentRefreshToken
         {
             get { return AuthLink.RefreshToken; }
@@ -38,6 +41,9 @@ namespace UnitystationLauncher.Models
         public void Store()
         {
             var json = JsonConvert.SerializeObject(AuthLink);
+            Console.WriteLine("Expires in: " + AuthLink.ExpiresIn);
+            Console.WriteLine("Refresh token: " + AuthLink.RefreshToken);
+
             using (StreamWriter writer = System.IO.File.CreateText("settings.json"))
             {
                 writer.WriteLine(json);
@@ -61,11 +67,56 @@ namespace UnitystationLauncher.Models
             authProvider.CreateUserWithEmailAndPasswordAsync(email, password, username, true);
 
         internal Task<User> GetUpdatedUser() => authProvider.GetUserAsync(AuthLink);
+
+        public async Task<bool> GetCustomToken(RefreshToken refreshToken)
+        {
+            var url = "https://api.unitystation.org/validatetoken?data=";
+
+            HttpRequestMessage r = new HttpRequestMessage(HttpMethod.Get, url + Uri.EscapeUriString(JsonConvert.SerializeObject(refreshToken)));
+
+            CancellationToken cancellationToken = new CancellationTokenSource(120000).Token;
+
+            HttpResponseMessage res;
+            try
+            {
+                res = await http.SendAsync(r, cancellationToken);
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            string msg = await res.Content.ReadAsStringAsync();
+            var response = JsonConvert.DeserializeObject<ApiResponse>(msg);
+            
+            if(response.errorCode != 0)
+            {
+                Console.WriteLine("Error: " + response.errorMsg);
+                return false;
+            }
+
+            return true;
+        }
     }
 
     public class LoginMsg
     {
         public string Email { get; set; }
         public string Pass { get; set; }
+    }
+
+    [Serializable]
+    public class RefreshToken
+    {
+        public string refreshToken;
+        public string userID;
+    }
+
+    [Serializable]
+    public class ApiResponse
+    {
+        public int errorCode = 0; //0 = all good, read the message variable now, otherwise read errorMsg
+        public string errorMsg;
+        public string message;
     }
 }
