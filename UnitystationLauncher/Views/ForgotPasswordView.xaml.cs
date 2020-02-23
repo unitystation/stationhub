b3386483ffd8096c69c09dd943836329fab08ad1
Doobly <rob@smashingpixels.com.au>
2020-02-23 11:30:24 +1000

    Updated to core 3.1 and added forgot pass link on login page

---
+++ UnitystationLauncher/Views/ForgotPasswordView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace UnitystationLauncher.Views
+{
+    public class ForgotPasswordView : UserControl
+    {
+        public ForgotPasswordView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
