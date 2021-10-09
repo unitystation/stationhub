a3ae568597f6c914afc85a8ffd2b3a4cffef4c2f
RedlineTriad <39059512+RedlineTriad@users.noreply.github.com>
2021-10-09 12:05:09 +0000

    Separate downloading from server to fix  #117 (#130)
    * Seperate downloading from server to fix  #117
    
    * Cleanup download code

--- UnitystationLauncher/Models/Download.cs
+++ UnitystationLauncher/Models/Download.cs
@@ -1,77 +1,118 @@
-﻿using Serilog;
-using System;
+﻿using System;
 using System.IO;
 using System.IO.Compression;
 using System.Net;
 using System.Reactive.Linq;
-using System.Reactive.Subjects;
 using System.Threading.Tasks;
+using ReactiveUI;
+using Serilog;
 
 namespace UnitystationLauncher.Models
 {
-    public class Download
+    public class Download : ReactiveObject
     {
-        public string Url { get; }
-        public string InstallationPath { get; }
-        public Subject<int> Progress { get; set; } = new Subject<int>();
-        public float Speed { get; set; }
-        public long Downloaded { get; set; }
-        public long Size { get; set; }
-        public long Time { get; set; }
+        private readonly string _url;
+        private readonly string _installationPath;
+        private long _size;
+        private bool _active;
+        private int _progress;
 
         public Download(string url, string installationPath)
         {
-            Url = url;
-            InstallationPath = installationPath;
+            _url = url;
+            _installationPath = installationPath;
+        }
+
+        public string Url => _url;
+
+        public string InstallationPath => _installationPath;
+
+        public long Size
+        {
+            get => _size;
+            set => this.RaiseAndSetIfChanged(ref _size, value);
+        }
+
+        public bool Active
+        {
+            get => _active;
+            set => this.RaiseAndSetIfChanged(ref _active, value);
+        }
+
+        public int Progress
+        {
+            get => _progress;
+            set => this.RaiseAndSetIfChanged(ref _progress, value);
         }
 
         public (string, int) ForkAndVersion => (ForkName, BuildVersion);
         public string ForkName => Installation.GetForkName(InstallationPath);
         public int BuildVersion => Installation.GetBuildVersion(InstallationPath);
-        public string RelativeInstallationPath => Path.GetRelativePath(Environment.CurrentDirectory, InstallationPath);
 
-        public async Task Start()
+        public async Task StartAsync()
         {
-            Log.Information("Download requested...");
-            Log.Information("Installation path: \"{Path}\"", InstallationPath);
+            Log.Information("Download requested, Installation Path '{Path}', Url '{Url}'", InstallationPath, Url);
 
+            if (!CanStart())
+            {
+                return;
+            }
+
+            Active = true;
+            try
+            {
+                Log.Information("Download started...");
+                var webRequest = WebRequest.Create(Url);
+                var webResponse = await webRequest.GetResponseAsync();
+                await using var responseStream = webResponse.GetResponseStream();
+                if (responseStream == null)
+                {
+                    Log.Error("Could not download from server");
+                    return;
+                }
+
+                Log.Information("Download connection established");
+                await using var progStream = new ProgressStream(responseStream);
+                Size = webResponse.ContentLength;
+
+                progStream.Progress
+                    .Select(p => (int)(p * 100 / Size))
+                    .DistinctUntilChanged()
+                    .Do(p => Log.Information("Progress: {Percentage}", p))
+                    .Subscribe(p => { Progress = p; });
+
+                await Task.Run(() =>
+                {
+                    Log.Information("Extracting...");
+                    try
+                    {
+                        var archive = new ZipArchive(progStream);
+                        archive.ExtractToDirectory(InstallationPath, true);
+
+                        Log.Information("Download completed");
+                        Installation.MakeExecutableExecutable(InstallationPath);
+                    }
+                    catch
+                    {
+                        Log.Information("Extracting stopped");
+                    }
+                });
+            }
+            finally
+            {
+                Active = false;
+            }
+        }
+
+        public bool CanStart()
+        {
             if (Directory.Exists(InstallationPath))
             {
                 Log.Information("Installation path already occupied");
-                return;
+                return false;
             }
 
-            Log.Information("Download URL: \"{Url}\"", Url);
-
-            Log.Information("Download started...");
-            var webRequest = WebRequest.Create(Url);
-            var webResponse = await webRequest.GetResponseAsync();
-            var responseStream = webResponse.GetResponseStream();
-            if (responseStream == null)
-            {
-                Log.Error("Could not get responseStream");
-                return;
-            }
-            Log.Information("Download connection established");
-            await using var progStream = new ProgressStream(responseStream);
-            var length = webResponse.ContentLength;
-            progStream.Progress
-                .Select(p => (int)(p * 100 / length))
-                .DistinctUntilChanged()
-                .Subscribe(p =>
-                {
-                    Progress.OnNext(p);
-                    Log.Information("Progress: {Prog}", p);
-                });
-
-            await Task.Run(() =>
-            {
-                Log.Information("Extracting...");
-                var archive = new ZipArchive(progStream);
-
-                archive.ExtractToDirectory(InstallationPath);
-                Log.Information("Download completed");
-            });
+            return true;
         }
     }
 }
