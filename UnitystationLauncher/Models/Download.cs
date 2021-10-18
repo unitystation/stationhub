9a3db86f669a88f377c0dab23a20f87806b4c35a
RedlineTriad <39059512+RedlineTriad@users.noreply.github.com>
2021-10-18 21:23:19 +0200

    Switch from deprecated WebRequest to HttpClient for downloads

--- UnitystationLauncher/Models/Download.cs
+++ UnitystationLauncher/Models/Download.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.IO;
 using System.IO.Compression;
-using System.Net;
+using System.Net.Http;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
+using Humanizer;
 using ReactiveUI;
 using Serilog;
 using UnitystationLauncher.Infrastructure;
@@ -50,7 +51,7 @@ namespace UnitystationLauncher.Models
         public string ForkName => Installation.GetForkName(InstallationPath);
         public int BuildVersion => Installation.GetBuildVersion(InstallationPath);
 
-        public async Task StartAsync()
+        public async Task StartAsync(HttpClient http)
         {
             Log.Information("Download requested, Installation Path '{Path}', Url '{Url}'", InstallationPath, Url);
 
@@ -63,9 +64,8 @@ namespace UnitystationLauncher.Models
             try
             {
                 Log.Information("Download started...");
-                var webRequest = WebRequest.Create(Url);
-                var webResponse = await webRequest.GetResponseAsync();
-                await using var responseStream = webResponse.GetResponseStream();
+                var request = await http.GetAsync(Url, HttpCompletionOption.ResponseHeadersRead);
+                await using var responseStream = await request.Content.ReadAsStreamAsync();
                 if (responseStream == null)
                 {
                     Log.Error("Could not download from server");
@@ -74,12 +74,14 @@ namespace UnitystationLauncher.Models
 
                 Log.Information("Download connection established");
                 await using var progStream = new ProgressStream(responseStream);
-                Size = webResponse.ContentLength;
+                Size = request.Content.Headers.ContentLength ??
+                       throw new NullReferenceException(nameof(request.Content.Headers.ContentLength));
+
+                using var logProgDisposable = LogProgress(progStream);
 
                 progStream.Progress
                     .Select(p => (int)(p * 100 / Size))
                     .DistinctUntilChanged()
-                    .Do(p => Log.Information("Progress: {Percentage}", p))
                     .Subscribe(p => { Progress = p; });
 
                 await Task.Run(() =>
@@ -103,6 +105,38 @@ namespace UnitystationLauncher.Models
             {
                 Active = false;
             }
+        }
+
+        private IDisposable LogProgress(ProgressStream progStream)
+        {
+            var lastPosition = 0L;
+            var lastTime = DateTime.Now;
+
+            return progStream.Progress
+                .Subscribe(pos =>
+                {
+                    var time = DateTime.Now;
+                    var deltaPos = pos - lastPosition;
+                    var deltaTime = time - lastTime;
+                    var deltaPercentage = deltaPos * 100 / Size;
+                    var percentage = pos * 100 / Size;
+
+                    if (pos != Size)
+                    {
+                        if (deltaPercentage < 25 && deltaTime.TotalSeconds < .25)
+                        {
+                            return;
+                        }
+                    }
+
+                    var speed = deltaPos.Bytes().Per(deltaTime);
+                    Log.Information("Progress: {ProgressPercent}%, Download speed = {DownloadSpeed}",
+                        percentage,
+                        speed.Humanize("#.##"));
+
+                    lastPosition = pos;
+                    lastTime = time;
+                });
         }
 
         public bool CanStart()
