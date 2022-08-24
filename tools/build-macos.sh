3904949c442dee4d0aff0b493b7de2c63eac6237
DeadLine <mishah8888@mail.ru>
2022-08-24 13:15:22 +0300

    Move info.plist and small update

--- tools/build-macos.sh
+++ tools/build-macos.sh
@@ -2,8 +2,8 @@
 
 APP_NAME="/Users/runner/publish/StationHub.app"
 PUBLISH_OUTPUT_DIRECTORY="/Users/runner/publish-macos/."
-INFO_PLIST="/Users/runner/work/stationhub/stationhub/UnitystationLauncher/Assets/Info.plist"
-ICON_FILE="/Users/runner/work/stationhub/stationhub/UnitystationLauncher/Assets/ian.ico"
+INFO_PLIST="/Users/runner/work/stationhub/stationhub/tools/Info.plist"
+ICON_FILE="/Users/runner/work/stationhub/stationhub/UnitystationLauncher/Assets/Ian.icns"
 
 if [ -d "$APP_NAME" ]
 then
@@ -18,5 +18,5 @@ mkdir "$APP_NAME/Contents/MacOS"
 mkdir "$APP_NAME/Contents/Resources"
 
 cp "$INFO_PLIST" "$APP_NAME/Contents/Info.plist"
-cp "$ICON_FILE" "$APP_NAME/Contents/Resources/ian.ico"
+cp "$ICON_FILE" "$APP_NAME/Contents/Resources/Ian.icns"
 cp -a "$PUBLISH_OUTPUT_DIRECTORY" "$APP_NAME/Contents/MacOS"
