bfda6189bb362f7f389977bb3e52a4de27f09a60
DeadLineSS13 <41364830+DeadLineSS13@users.noreply.github.com>
2022-08-24 11:37:59 +0300

    Update build-macos.sh

--- tools/build-macos.sh
+++ tools/build-macos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 APP_NAME="/Users/runner/publish/StationHub.app"
+PUBLISH_OUTPUT_DIRECTORY="/Users/runner/work/stationhub/stationhub/UnitystationLauncher/bin/Release/netcoreapp3.1/osx-x64/."
 INFO_PLIST="/Users/runner/work/stationhub/stationhub/UnitystationLauncher/Assets/Info.plist"
 ICON_FILE="/Users/runner/work/stationhub/stationhub/UnitystationLauncher/Assets/ian.ico"
 
@@ -17,3 +18,4 @@ mkdir "$APP_NAME/Contents/Resources"
 
 cp "$INFO_PLIST" "$APP_NAME/Contents/Info.plist"
 cp "$ICON_FILE" "$APP_NAME/Contents/Resources/ian.ico"
+cp -a "$PUBLISH_OUTPUT_DIRECTORY" "$APP_NAME/Contents/MacOS"
