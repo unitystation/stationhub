f4db92263c0b9add3c2712ae781db0b0d334a4dd
DeadLineSS13 <41364830+DeadLineSS13@users.noreply.github.com>
2022-08-24 12:39:17 +0300

    Update build-macos.sh

--- tools/build-macos.sh
+++ tools/build-macos.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 APP_NAME="/Users/runner/publish/StationHub.app"
+PUBLISH_OUTPUT_DIRECTORY="/Users/runner/publish-macos/StationHub.app/"
 INFO_PLIST="/Users/runner/work/stationhub/stationhub/UnitystationLauncher/Assets/Info.plist"
 ICON_FILE="/Users/runner/work/stationhub/stationhub/UnitystationLauncher/Assets/ian.ico"
 
+if [ -d "$APP_NAME" ]
+then
+    rm -rf "$APP_NAME"
+fi
+
+mkdir "$APP_NAME"
+
+mkdir "$APP_NAME/Contents"
+mkdir "$APP_NAME/Contents/MacOS"
 mkdir "$APP_NAME/Contents/Resources"
 
 cp "$INFO_PLIST" "$APP_NAME/Contents/Info.plist"
 cp "$ICON_FILE" "$APP_NAME/Contents/Resources/ian.ico"
+cp -a "$PUBLISH_OUTPUT_DIRECTORY" "$APP_NAME/Contents/MacOS"
