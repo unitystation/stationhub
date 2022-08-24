5d222f65f99249712230ea136f79a6e69c285b30
DeadLine <mishah8888@mail.ru>
2022-08-24 13:26:17 +0300

    Update script

--- tools/build-macos.sh
+++ tools/build-macos.sh
@@ -3,7 +3,9 @@
 APP_NAME="/Users/runner/publish/StationHub.app"
 PUBLISH_OUTPUT_DIRECTORY="/Users/runner/publish-macos/."
 INFO_PLIST="/Users/runner/work/stationhub/stationhub/tools/Info.plist"
+ENTITLEMENTS="/Users/runner/work/stationhub/stationhub/tools/MyAppEntitlements.entitlements"
 ICON_FILE="/Users/runner/work/stationhub/stationhub/UnitystationLauncher/Assets/Ian.icns"
+SIGNING_IDENTITY="Developer ID: MyCompanyName" # matches Keychain Access certificate name
 
 if [ -d "$APP_NAME" ]
 then
@@ -20,3 +22,14 @@ mkdir "$APP_NAME/Contents/Resources"
 cp "$INFO_PLIST" "$APP_NAME/Contents/Info.plist"
 cp "$ICON_FILE" "$APP_NAME/Contents/Resources/Ian.icns"
 cp -a "$PUBLISH_OUTPUT_DIRECTORY" "$APP_NAME/Contents/MacOS"
+
+find "$APP_NAME/Contents/MacOS/"|while read fname; do
+    if [[ -f $fname ]]; then
+        echo "[INFO] Signing $fname"
+        codesign --force --timestamp --options=runtime --entitlements "$ENTITLEMENTS" --sign "$SIGNING_IDENTITY" "$fname"
+    fi
+done
+
+echo "[INFO] Signing app file"
+
+codesign --force --timestamp --options=runtime --entitlements "$ENTITLEMENTS" --sign "$SIGNING_IDENTITY" "$APP_NAME"
