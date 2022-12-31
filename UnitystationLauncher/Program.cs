0a80da92a6a9f0941cadf68e564ac5030fe203ea
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-12-31 22:44:57 +0200

    upgrade to .NET 6

--- UnitystationLauncher/Program.cs
+++ UnitystationLauncher/Program.cs
@@ -1,24 +1,22 @@
 using Avalonia;
 using Avalonia.ReactiveUI;
 
-namespace UnitystationLauncher
+namespace UnitystationLauncher;
+static class Program
 {
-    static class Program
-    {
-        public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
 
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-                .With(new X11PlatformOptions { UseGpu = true })
-                .With(new AvaloniaNativePlatformOptions { UseGpu = true, UseDeferredRendering = false })
-                .With(new MacOSPlatformOptions { ShowInDock = true })
-                .With(new Win32PlatformOptions
-                {
-                    UseDeferredRendering = false
-                })
-                .LogToTrace()
-                .UseReactiveUI();
-    }
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .With(new X11PlatformOptions { UseGpu = true })
+            .With(new AvaloniaNativePlatformOptions { UseGpu = true, UseDeferredRendering = false })
+            .With(new MacOSPlatformOptions { ShowInDock = true })
+            .With(new Win32PlatformOptions
+            {
+                UseDeferredRendering = false
+            })
+            .LogToTrace()
+            .UseReactiveUI();
 }
