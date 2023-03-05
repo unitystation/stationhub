c1b359ccbe0d52ee86e3754bb75494172be903f3
Nickolas Gupton <nickolas@gupton.xyz>
2023-03-05 13:09:17 -0600

    Make changes clickable to open the PR

--- UnitystationLauncher/ViewModels/ChangeViewModel.cs
+++ UnitystationLauncher/ViewModels/ChangeViewModel.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using UnitystationLauncher.Models.Api.Changelog;
 
 namespace UnitystationLauncher.ViewModels;
@@ -7,11 +8,27 @@ public class ChangeViewModel : ViewModelBase
     private string Type { get; }
     private string Description { get; }
     private string AuthorAndPr { get; }
+    private string? PrUrl { get; }
 
     public ChangeViewModel(Change change)
     {
         Type = $"[{change.Category}]";
         Description = change.Description ?? "null";
         AuthorAndPr = $"{change.AuthorUsername ?? "null"} | PR #{change.PrNumber}";
+        PrUrl = change.PrUrl;
+    }
+
+    private void OnClick()
+    {
+        if (PrUrl != null)
+        {
+            ProcessStartInfo psi = new()
+            {
+                FileName = PrUrl,
+                UseShellExecute = true
+            };
+
+            Process.Start(psi);
+        }
     }
 }
