2fdceba61c5093254b9c38110c28f78c5ddd0921
Gilles <43683714+corp-0@users.noreply.github.com>
2023-06-03 05:43:13 -0400

    chore: bumps version in preparation for new release (#207)

--- UnitystationLauncher/Constants/AppInfo.cs
+++ UnitystationLauncher/Constants/AppInfo.cs
@@ -4,5 +4,5 @@ public static class AppInfo
 {
     // Whenever you change the currentBuild here, please also update the one in
     // UnitystationLauncher/Assets/org.unitystation.StationHub.metainfo.xml
-    public const int CurrentBuild = 931;
+    public const int CurrentBuild = 932;
 }
