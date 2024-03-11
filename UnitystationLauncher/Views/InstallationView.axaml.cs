ffeb2b1fff8dc6232823dac4d28dd6dc582db9cd
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2024-03-11 22:46:48 +0200

    Allow paths in the installations page to be clickable

--- UnitystationLauncher/Views/InstallationView.axaml.cs
+++ UnitystationLauncher/Views/InstallationView.axaml.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Diagnostics;
+using System.IO;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 namespace UnitystationLauncher.Views;
@@ -13,5 +17,38 @@ public class InstallationView : UserControl
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+    
+    private void HandlePathClick(object sender, PointerPressedEventArgs e)
+    {
+        TextBlock? block = sender as TextBlock;
+        if (block?.Text == null) return;
+        string path = Path.GetFullPath(block.Text);
+        string protcol = GetManagerProtocl();
+        Process.Start(protcol, new Uri(RemoveLastPart(path)).ToString());
+    }
+
+    private string GetManagerProtocl()
+    {
+        string protocol = "Explorer.exe";
+        if (OperatingSystem.IsWindows())
+        {
+            protocol = "Explorer.exe";
+        }
+        else if (OperatingSystem.IsMacOS())
+        {
+            protocol = "open";
+        }
+        else if (OperatingSystem.IsLinux())
+        {
+            protocol = "xdg-open";
+        }
+        return protocol;
+    }
+
+    private string RemoveLastPart(string path)
+    {
+        int lastBackslashIndex = path.LastIndexOf('\\');
+        return lastBackslashIndex >= 0 ? path.Substring(0, lastBackslashIndex) : path;
     }
 }
