bf7a19a1f58a245201da5ea9588a2e6daa419462
Maximal98 <55159959+Maximal98@users.noreply.github.com>
2025-03-31 01:58:09 +0200

    Move settings from Installations to Preferences (#242)
    * Move settings from installations to preferences
    
    * Remove Build Number from Installations
    
    * chore: dotnet-format stuff
    
    ---------
    
    Co-authored-by: Gilles <43683714+corp-0@users.noreply.github.com>

--- UnitystationLauncher/ViewModels/InstallationsPanelViewModel.cs
+++ UnitystationLauncher/ViewModels/InstallationsPanelViewModel.cs
@@ -5,15 +5,9 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
-using System.Threading.Tasks;
-using MsBox.Avalonia.Base;
 using Serilog;
 using UnitystationLauncher.Constants;
-using UnitystationLauncher.Infrastructure;
 using UnitystationLauncher.Models;
-using UnitystationLauncher.Models.ConfigFile;
-using UnitystationLauncher.Models.Enums;
-using UnitystationLauncher.Services;
 using UnitystationLauncher.Services.Interface;
 
 namespace UnitystationLauncher.ViewModels
@@ -22,132 +16,16 @@ namespace UnitystationLauncher.ViewModels
     {
         public override string Name => "Installations";
         public override bool IsEnabled => true;
-
-        private string? _buildNum;
-        public string? BuildNum
-        {
-            get => _buildNum;
-            set => this.RaiseAndSetIfChanged(ref _buildNum, value);
-        }
-
-        private bool _autoRemove;
-        public bool AutoRemove
-        {
-            get => _autoRemove;
-            set => this.RaiseAndSetIfChanged(ref _autoRemove, value);
-        }
-
-        private bool? _TTSEnabled;
-
-        public bool? TTSEnabled
-        {
-            get => _TTSEnabled;
-            set => this.RaiseAndSetIfChanged(ref _TTSEnabled, value);
-        }
-
         public ObservableCollection<InstallationViewModel> InstallationViews { get; init; } = new();
 
         private readonly TimeSpan _refreshInterval = TimeSpan.FromSeconds(2);
-        private readonly IPreferencesService _preferencesService;
         private readonly IInstallationService _installationService;
-        private readonly IEnvironmentService _environmentService;
-        private readonly ITTSService _ttsService;
 
-        public InstallationsPanelViewModel(IInstallationService installationService,
-            IPreferencesService preferencesService,
-            IEnvironmentService environmentService,
-            ITTSService ttsService
-            )
+        public InstallationsPanelViewModel(IInstallationService installationService)
         {
             _installationService = installationService;
-            _preferencesService = preferencesService;
-            _environmentService = environmentService;
-
-            _ttsService = ttsService;
-
-            BuildNum = $"Hub Build Num: {AppInfo.CurrentBuild}";
-
-            UpdateFromPreferences();
-
-            this.WhenAnyValue(p => p.AutoRemove)
-                .Select(_ => Observable.FromAsync(OnAutoRemoveChangedAsync))
-                .Concat()
-                .Subscribe();
-
-            this.WhenAnyValue(p => p.TTSEnabled)
-                .Select(_ => Observable.FromAsync(OnTTSChange))
-                .Concat()
-                .Subscribe();
-
 
             InitializeInstallationsList();
-        }
-
-        private void UpdateFromPreferences()
-        {
-            Preferences prefs = _preferencesService.GetPreferences();
-            AutoRemove = prefs.AutoRemove;
-            TTSEnabled = prefs.TTSEnabled;
-        }
-
-        private async Task OnAutoRemoveChangedAsync()
-        {
-
-
-            if (AutoRemove)
-            {
-                IMsBox<string> msgBox = MessageBoxBuilder.CreateMessageBox(MessageBoxButtons.YesNo,
-                    "Are you sure?", "This will remove older installations from disk. Proceed?");
-
-                string response = await msgBox.ShowAsync();
-                if (response.Equals(MessageBoxResults.Yes))
-                {
-                    SaveChoice();
-                }
-                else
-                {
-                    AutoRemove = false;
-                }
-            }
-            else
-            {
-                SaveChoice();
-            }
-        }
-
-        private async Task OnTTSChange()
-        {
-            if (_environmentService.GetCurrentEnvironment() == CurrentEnvironment.MacOsStandalone)
-            {
-                IMsBox<string> msgBox = MessageBoxBuilder.CreateMessageBox(MessageBoxButtons.Ok,
-                    " Mac Support is sad ",
-                    " Sadly TTS is unsupported on Mac, If you'd like to contribute this, feel free to shoot a message on the Discord. ");
-                string response = await msgBox.ShowAsync();
-                return;
-            }
-
-
-            if (TTSEnabled is false)
-            {
-                IMsBox<string> msgBox = MessageBoxBuilder.CreateMessageBox(MessageBoxButtons.YesNo,
-                    "Are you sure?", "This will disable Character voices (TTS). Proceed? (If yes It can take a little bit to delete So be patient)");
-
-                string response = await msgBox.ShowAsync();
-                if (response.Equals(MessageBoxResults.Yes))
-                {
-                    TTSEnabled = false;
-                    SaveChoiceTTS();
-                }
-                else
-                {
-                    TTSEnabled = true;
-                    SaveChoiceTTS();
-                }
-            }
-            else
-            {
-                SaveChoiceTTS();
-            }
         }
 
         private void InitializeInstallationsList()
@@ -200,30 +78,6 @@ namespace UnitystationLauncher.ViewModels
                     InstallationViews.Remove(viewModel);
                 }
             }
-        }
-
-        private void SaveChoice()
-        {
-            Preferences prefs = _preferencesService.GetPreferences();
-            prefs.AutoRemove = AutoRemove;
-        }
-
-        private void SaveChoiceTTS()
-        {
-            Preferences prefs = _preferencesService.GetPreferences();
-            prefs.TTSEnabled = TTSEnabled;
-
-            if (TTSEnabled == false)
-            {
-                _ttsService.StopTTS();
-                string installationBasePath = _preferencesService.GetPreferences().InstallationPath;
-                var LocalVersion = System.IO.Path.Combine(installationBasePath, "tts");
-                if (System.IO.Directory.Exists(LocalVersion))
-                {
-                    System.IO.Directory.Delete(LocalVersion, true);
-                }
-            }
-
         }
 
         public override void Refresh()
--- UnitystationLauncher/ViewModels/PreferencesPanelViewModel.cs
+++ UnitystationLauncher/ViewModels/PreferencesPanelViewModel.cs
@@ -1,11 +1,15 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
 using MsBox.Avalonia.Base;
 using ReactiveUI;
 using Serilog;
 using UnitystationLauncher.Constants;
 using UnitystationLauncher.Infrastructure;
-using UnitystationLauncher.Models.Enums;
 using UnitystationLauncher.Services.Interface;
+using UnitystationLauncher.Models.Enums;
+using UnitystationLauncher.Models.ConfigFile;
+using DynamicData.Binding;
 
 namespace UnitystationLauncher.ViewModels
 {
@@ -21,16 +25,105 @@ namespace UnitystationLauncher.ViewModels
             get => _installationPath;
             set => this.RaiseAndSetIfChanged(ref _installationPath, value);
         }
+        private bool _autoRemove;
+        public bool AutoRemove
+        {
+            get => _autoRemove;
+            set => this.RaiseAndSetIfChanged(ref _autoRemove, value);
+        }
+        // for a future overhaul of TTS
+        // private string TTSPath;
+        // public string TTSText
+        // { // exp ? true : false
+        //     get { return string.IsNullOrEmpty(TTSPath) ? "Not Installed." : $"Is Installed at {TTSPath}"; }
+        // }
+        // public string TTSButtonText
+        // {
+        //     get { return string.IsNullOrEmpty(TTSPath) ? "Install" : "Uninstall"; }
+        // }
+        private bool? _TTSEnabled;
+
+        public bool? TTSEnabled
+        {
+            get => _TTSEnabled;
+            set => this.RaiseAndSetIfChanged(ref _TTSEnabled, value);
+        }
 
         private readonly IPreferencesService _preferencesService;
         private readonly IInstallationService _installationService;
+        private readonly IEnvironmentService _environmentService;
+        private readonly ITTSService _ttsService;
 
-        public PreferencesPanelViewModel(IPreferencesService preferencesService, IInstallationService installationService)
+        public PreferencesPanelViewModel(
+            IPreferencesService preferencesService,
+            IInstallationService installationService,
+            IEnvironmentService environmentService,
+            ITTSService ttsService)
         {
             _preferencesService = preferencesService;
             _installationService = installationService;
+            _environmentService = environmentService;
+            _ttsService = ttsService;
 
-            _installationPath = _preferencesService.GetPreferences().InstallationPath;
+            Preferences preferences = _preferencesService.GetPreferences();
+            _installationPath = preferences.InstallationPath;
+            _autoRemove = preferences.AutoRemove;
+            _TTSEnabled = preferences.TTSEnabled;
+            this.WhenAnyValue(p => p.AutoRemove)
+                .Select(_ => Observable.FromAsync(OnAutoRemoveChangedAsync))
+                .Concat()
+                .Subscribe();
+
+            this.WhenAnyValue(p => p.TTSEnabled)
+                .Select(_ => Observable.FromAsync(OnTTSChangedAsync))
+                .Concat()
+                .Subscribe();
+        }
+
+        public async Task OnAutoRemoveChangedAsync()
+        {
+            if (AutoRemove)
+            {
+                IMsBox<string> msgBox = MessageBoxBuilder.CreateMessageBox(MessageBoxButtons.YesNo,
+                    "Are you sure?", "This will remove older installations from disk. Proceed?");
+
+                string response = await msgBox.ShowAsync();
+                AutoRemove = response.Equals(MessageBoxResults.Yes);
+            }
+            _preferencesService.GetPreferences().AutoRemove = AutoRemove;
+        }
+
+        public async Task OnTTSChangedAsync()
+        {
+            if (_environmentService.GetCurrentEnvironment() == CurrentEnvironment.MacOsStandalone)
+            {
+                IMsBox<string> msgBox = MessageBoxBuilder.CreateMessageBox(MessageBoxButtons.Ok,
+                    " MacOS is unsupported ",
+                    " Text-To-Speech is not supported on MacOS. If you would like to contribute, talk to us on the discord. ");
+                await msgBox.ShowAsync(); //should this even be awaited?
+                return;
+            }
+
+
+            if (TTSEnabled is false)
+            {
+                IMsBox<string> msgBox = MessageBoxBuilder.CreateMessageBox(MessageBoxButtons.YesNo,
+                    "Are you sure?", "This will disable Character voices and delete the TTS System. This may take a While.");
+                string response = await msgBox.ShowAsync();
+                TTSEnabled = response.Equals(MessageBoxResults.No);
+            }
+            _preferencesService.GetPreferences().TTSEnabled = TTSEnabled;
+
+            if (TTSEnabled == false)
+            {
+                _ttsService.StopTTS();
+                string installationBasePath = _preferencesService.GetPreferences().InstallationPath; //yucky
+                var LocalVersion = System.IO.Path.Combine(installationBasePath, "tts");
+                if (System.IO.Directory.Exists(LocalVersion))
+                {
+                    System.IO.Directory.Delete(LocalVersion, true);
+                }
+            }
         }
 
         public async Task SetInstallationPathAsync(string path)
